aab42fc840dc6e3950d9d762da40fdd4c16fa831
liguohua <liguohua@cloud-star.com.cn>
2017-10-27 00:29:25 +0800

    add docker

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {ProductionComponent} from "./production/production.component";
+import {Page404Component} from "./page404/page404.component";
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'production',component:ProductionComponent},
+  {path:'**',component:Page404Component},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductionComponent } from './production/production.component';
+import { Page404Component } from './page404/page404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProductionComponent
+    ProductionComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
